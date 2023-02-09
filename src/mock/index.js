55c207d91acb200d576277a4a85592f1d2a998ce
singledeveloper63 <singledeveloper63@gmail.com>
2023-02-09 08:23:54 +0500

    App initialized with creating folder structure

---
+++ src/mock/index.js
@@ -0,0 +1,1 @@
+export { mockFoods } from './mock-data';
---
+++ src/mock/mock-data.js
@@ -0,0 +1,32 @@
+export const mockFoods = [
+  {
+    id: 'f1',
+    name: 'Original Salad',
+    price: 8,
+    restaurant: 'Lovy Food'
+  },
+  {
+    id: 'f2',
+    name: 'Fresh Salad',
+    price: 10,
+    restaurant: 'Cloudy Resto'
+  },
+  {
+    id: 'f3',
+    name: 'Yummie Ice Cream',
+    price: 6,
+    restaurant: 'Circlo Resto'
+  },
+  {
+    id: 'f4',
+    name: 'Vegan Special',
+    price: 11,
+    restaurant: 'Haty Food'
+  },
+  {
+    id: 'f5',
+    name: 'Mixed Pasta',
+    price: 13,
+    restaurant: 'Recto Food'
+  }
+];
